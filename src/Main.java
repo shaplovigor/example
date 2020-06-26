f36d3bdd5da3ee6e5b3162292df8f2b4486b5d56
ExampleAccountSkillFactory <skill.email.example@gmail.com>
2020-06-26 05:06:42 +0300

    Add strings.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s1 = new String("Строка тоже объект");
+        String s2 = "Строка тоже объект";
+
     }
+
 }
